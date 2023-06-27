652efb15b36d3c65134313e60a55110d116a56f6
Lisbon <lisbon@dublin.com>
2023-06-27 09:59:19 +0300

    Fetch with for - OOP

--- js/list.js
+++ js/list.js
@@ -2,20 +2,46 @@ import { renderHeader } from "./header.js";
 
 renderHeader(false);
 
-const goods = [
-    { name: 'Morka', price: 1, },
-    { name: 'Kopustas', price: 1.5, },
-    { name: 'Svogunas', price: 2, },
-    { name: 'Agurkas', price: 3, },
-    { name: 'Pomidoras', price: 4.5, },
-    { name: 'Bulve', price: 1.29, },
-    { name: 'Kalafioras', price: 102.99, },
-];
+// const goods = [
+//     { name: 'Morka', price: 1, },
+//     { name: 'Kopustas', price: 1.5, },
+//     { name: 'Svogunas', price: 2, },
+//     { name: 'Agurkas', price: 3, },
+//     { name: 'Pomidoras', price: 4.5, },
+//     { name: 'Bulve', price: 1.29, },
+//     { name: 'Kalafioras', price: 102.99, },
+// ];
 
+// const listDOM = document.querySelector('ul');
+
+// for (const { name, price } of goods) {
+//     const formatedPrice = price.toFixed(2).replace('.', ',');
+
+//     listDOM.innerHTML += `<li>${name} (${formatedPrice} Eur)</li>`;
+// }
+
+const url = 'http://localhost:4510/data/goods2.json';
 const listDOM = document.querySelector('ul');
+let HTML = '';
 
-for (const { name, price } of goods) {
-    const formatedPrice = price.toFixed(2).replace('.', ',');
+if (listDOM) {
+    try {
+        const response = await fetch(url);
+        const goods = await response.json();
 
-    listDOM.innerHTML += `<li>${name} (${formatedPrice} Eur)</li>`;
+        for (const { name, price } of goods) {
+            const formatedPrice = price.toFixed(2).replace('.', ',');
+            HTML += `<li>${name} (${formatedPrice} Eur)</li>`;
+        }
+    } catch (error) {
+        HTML = '<p>ERROR: kazkas blogai...</p>';
+    } finally {
+        listDOM.innerHTML = HTML;
+    }
 }
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log('>>>', sum(7, 5));
